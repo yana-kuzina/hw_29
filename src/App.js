c50a16bbc58f012d7baa8f200115ecd69941177e
Yana Kuzina <kuzina25.10.2013@gmail.com>
2022-06-30 20:05:07 +0300

    added Dialog for active image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import ImagesList from "./components/ImagesList";
 
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+
 function App() {
-  return <ImagesList />;
+  return (
+    <div>
+      <ImagesList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ImagesList.js
+++ src/components/ImagesList.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import Box from "@mui/material/Box";
+
 import axios from "../helpers/axios";
 
 const ImagesList = () => {
   const [images, setImages] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [activeImage, setActiveImage] = useState(null);
 
   useEffect(() => {
     axios.get("/list", { params: { limit: 10, page } }).then((data) => {
@@ -18,11 +24,21 @@ const ImagesList = () => {
     setPage(page + 1);
   };
 
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setActiveImage(null);
+  };
+
+  const handleDialogOpen = (image) => () => {
+    setDialogOpen(true);
+    setActiveImage(image);
+  };
+
   return (
     <div className="images-list">
       {images.map((image) => (
         <div className="image-box" key={image.id}>
-          <img src={image.download_url} />
+          <img onClick={handleDialogOpen(image)} src={image.download_url} />
         </div>
       ))}
 
@@ -31,6 +47,15 @@ const ImagesList = () => {
       ) : (
         <button onClick={handleShowMore}>Show more</button>
       )}
+
+      <Dialog open={isDialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>About image</DialogTitle>
+        <Box sx={{ p: 2 }}>
+          <p>author: {activeImage?.author}</p>
+          <p>width: {activeImage?.width}px</p>
+          <p>height: {activeImage?.height}px</p>
+        </Box>
+      </Dialog>
     </div>
   );
 };
